ab0b1d3045efdfa0d6aeb6793fe76a8b5200d7bb
Alexander Bersenev <bay@hackerdom.ru>
2018-10-29 01:28:31 +0500

    infrastructure: try to redisign trafic dumps once again

--- ansible/roles/vpn/files/trafdump/add_trafdump_rules.sh
+++ ansible/roles/vpn/files/trafdump/add_trafdump_rules.sh
@@ -2,16 +2,19 @@
 # adds rules for traffic dump
 # this script should be run once before the game starts
 
+sysctl net.core.rmem_max=2000000000
+echo "sysctl net.core.rmem_max=2000000000"
+
 for chain in INPUT FORWARD OUTPUT; do
-    if ! iptables -t mangle -C $chain -s 10.80.0.0/14 -j TEE --gateway 10.10.10.30 &>/dev/null; then
-        iptables -t mangle -A $chain -s 10.80.0.0/14 -j TEE --gateway 10.10.10.30
+    if ! iptables -t mangle -C $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
     fi
 
-    if ! iptables -t mangle -C $chain -s 10.60.0.0/14 -j TEE --gateway 10.10.10.30 &>/dev/null; then
-        iptables -t mangle -A $chain -s 10.60.0.0/14 -j TEE --gateway 10.10.10.30
+    if ! iptables -t mangle -C $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
     fi
 
-    if ! iptables -t mangle -C $chain -s 10.10.10.0/28 -j TEE --gateway 10.10.10.30 &>/dev/null; then
-        iptables -t mangle -A $chain -s 10.10.10.0/28 -j TEE --gateway 10.10.10.30
+    if ! iptables -t mangle -C $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100
     fi
 done
--- ansible/roles/vpn/files/trafdump/del_trafdump_rules.sh
+++ ansible/roles/vpn/files/trafdump/del_trafdump_rules.sh
@@ -3,7 +3,7 @@
 # this script shouldn't be run normally :)
 
 for chain in INPUT FORWARD OUTPUT; do
-    iptables -t mangle -D $chain -s 10.80.0.0/14 -j TEE --gateway 10.10.10.30
-    iptables -t mangle -D $chain -s 10.60.0.0/14 -j TEE --gateway 10.10.10.30
-    iptables -t mangle -D $chain -s 10.10.10.0/28 -j TEE --gateway 10.10.10.30
+    iptables -t mangle -D $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
+    iptables -t mangle -D $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
+    iptables -t mangle -D $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100
 done
