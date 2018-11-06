6304abbdf6496e0aca30161b8b562bcaab70e753
Krait <krait@skbkontur.ru>
2018-11-06 22:31:11 +0500

    partychat: added a stub for master.

---
+++ services/partychat/Master/PartyChat.Master/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PartyChat.Master
+{
+    internal class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
