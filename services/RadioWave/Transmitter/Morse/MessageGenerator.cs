8776ffd474f28c7f30befd520e49ec67dc962925
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2018-11-07 20:20:32 +0500

    RadioWave[Transmitter]: remove useless

--- services/RadioWave/Transmitter/Morse/MessageGenerator.cs
+++ services/RadioWave/Transmitter/Morse/MessageGenerator.cs
@@ -5,35 +5,15 @@ namespace Transmitter.Morse
 {
 	public class MessageGenerator : IEnumerator<double>
 	{
-		private readonly Message message;
-		private readonly StringInfiniteEnumerator textStream;
 		private readonly SignalToPcmConverter pcmStream;
 
 		public MessageGenerator(Message message, int rate)
 		{
-			this.message = message;
-			textStream = new StringInfiniteEnumerator(message.Text);
+			var textStream = new StringInfiniteEnumerator(message.Text);
 			var morseStream = new StringToMorseConverter(textStream);
 			var signalStream = new MorseToSignalConverter(morseStream);
 			pcmStream = new SignalToPcmConverter(signalStream, message.Frequency, 60_000.0 / message.DPM, 1.0 / rate);
 		}
-
-		private sealed class MessageGeneratorComparer : IEqualityComparer<MessageGenerator>
-		{
-			public bool Equals(MessageGenerator x, MessageGenerator y)
-			{
-				if (ReferenceEquals(x, y)) return true;
-				if (ReferenceEquals(x, null)) return false;
-				if (ReferenceEquals(y, null)) return false;
-				if (x.GetType() != y.GetType()) return false;
-				return Message.Comparer.Equals(x.message, y.message);
-			}
-
-			public int GetHashCode(MessageGenerator obj) 
-				=> Message.Comparer.GetHashCode(obj.message);
-		}
-
-		public static readonly IEqualityComparer<MessageGenerator> Comparer = new MessageGeneratorComparer();
 
 		public bool MoveNext()
 			=> pcmStream.MoveNext();
