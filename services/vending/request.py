eae013727a7a8afcac0276715ed95ed322c429c7
rx00 <it.tec@me.com>
2018-11-08 02:52:26 +0500

    machines meat v1

--- services/vending/request.py
+++ services/vending/request.py
@@ -5,12 +5,12 @@ from typing import Tuple
 
 
 class Action(enum.Enum):  # todo remove comments
-    CREATE_MACHINE = 100  # -> nothing to new machine (NONE -> id) (inc id, anything pub)
-    GET_MACHINE_INFO = 101  # name [0-15] & inventor[16-31] (id -> 32 bits)
-    GET_PRIVATE_MACHINE_INFO = 102  # key[256-383] m_master[384-512] (id, key -> master)
-    GET_META = 103  # (id, meta-point -> bit)
-    CREATE_VENDING_KEYS = 110  # (id, k1-v1, ..., kN-vN -> key1, ..., keyN)
-    GET_DATA_BY_VENDING_KEY = 111  # (id, k -> bit)
+    CREATE_MACHINE = 0  # -> nothing to new machine (NONE -> id) (inc id, anything pub)
+    GET_MACHINE_INFO = 1  # name [0-15] & inventor[16-31] (id -> 32 bits)
+    GET_PRIVATE_MACHINE_INFO = 2  # key[256-383] m_master[384-512] (id, key -> master)
+    GET_META = 3  # (id, meta-point -> bit)
+    CREATE_VENDING_KEYS = 4  # (id, k1-v1, ..., kN-vN -> key1, ..., keyN)
+    GET_DATA_BY_VENDING_KEY = 5  # (id, k -> bit)
 
 
 @dataclasses.dataclass
@@ -18,5 +18,6 @@ class Request:
     action: Action
     args: Tuple[str]
 
-    def unpack(self):
+    @staticmethod
+    def unpack():
         pass
