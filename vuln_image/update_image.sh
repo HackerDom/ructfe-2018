c19ea90860cb2b80a506f2faa4a5e54489c9a2f5
Alexander Ankudinov <xelez@yandex-team.ru>
2018-11-08 20:37:36 +0500

    Update scripts

--- vuln_image/update_image.sh
+++ vuln_image/update_image.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function wait_for_ssh () {
+    until ssh -o ConnectTimeout=2 -p2222 root@localhost echo 'Image is up'
+        do sleep 1
+    done
+}
+
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 VBOX="ructfe2018-master"
 
@@ -8,9 +14,17 @@ set -u
 set -x
 
 cd $BASE_DIR
+
+# Start vm
 vboxmanage startvm $VBOX --type headless
-sleep 8
+wait_for_ssh
+
+# Deploy updates
 ansible-playbook -i ansible_hosts setup.yml
+
+# Power off VM
 ssh -p2222 localhost poweroff
 sleep 8
+
+# Export image
 time vboxmanage export "ructfe2018-master" -o ructfe2018-tmp.ova
