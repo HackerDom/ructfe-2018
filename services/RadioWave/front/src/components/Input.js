79fcaf42a5b6cba6ba31355495b7b35b411d80e6
Zaynullin Bulat <zaynullin_bulat@skbkontur.ru>
2018-11-08 20:16:04 +0500

    RadioWave[front]: done

--- services/RadioWave/front/src/components/Input.js
+++ services/RadioWave/front/src/components/Input.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import './Input.css'
+import React from "react";
+import "./Input.css";
 
-const Input = ({field}) => {
-    return <label className={"Input-label"} htmlFor={field.name}>{field.name}: <input className={"Input"} {...field} /></label>
-}
-
-export default Input
+const Input = ({ field, onChangeChannel, form }) => {
+  const onChange = (...args) => {
+    if (onChangeChannel) {
+      onChangeChannel(...args);
+    }
+    form.handleChange(...args);
+  };
+  return (
+    <label className={"Input-label"} htmlFor={field.name}>
+      {field.name}: <input className={"Input"} {...field} onChange={onChange} />
+    </label>
+  );
+};
+export default Input;
