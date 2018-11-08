f66befc5f56290f452bb829b145f5d3668b6c99d
Alexander Ankudinov <xelez@yandex-team.ru>
2018-11-08 20:27:52 +0500

    Update scripts

--- vuln_image/update_image.sh
+++ vuln_image/update_image.sh
@@ -9,7 +9,8 @@ set -x
 
 cd $BASE_DIR
 vboxmanage startvm $VBOX --type headless
+sleep 8
 ansible-playbook -i ansible_hosts setup.yml
 ssh -p2222 localhost poweroff
-sleep 5
+sleep 8
 time vboxmanage export "ructfe2018-master" -o ructfe2018-tmp.ova
