e19bf07549133e809d6f355f07fbc3bde5729aed
rx00 <it.tec@me.com>
2018-11-10 04:24:14 +0500

    type errs too

--- services/vending/handlers/create_vending_keys_handler.py
+++ services/vending/handlers/create_vending_keys_handler.py
@@ -12,5 +12,5 @@ class CreateVendingKeysHandler(BaseHandler):
             data_in_vending = request.body.readline().decode().strip()[:32]
             vm_pub, vm_private = self.vm_keys.add_new_vending_machine_items(data_in_vending, 8)
             return Response(200, f"{vm_pub}:{':'.join(vm_private)}".encode())
-        except ValueError:
+        except (TypeError, ValueError):
             return Response(400, b'Bad Request')
