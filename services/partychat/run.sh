f7bd7a0186bc7882b96dd0eb9e8e5c30ec320e86
DQKrait <krait@dqteam.org>
2018-11-09 22:50:57 +0000

    partychat: added a sanity check in run.sh

--- services/partychat/run.sh
+++ services/partychat/run.sh
@@ -1,9 +1,14 @@
 #! /bin/bash
 
-ip=`hostname -I | grep -Po '\d+\.\d+\.\d+\.\d+'`
+ip=`hostname -I | grep -Po '10\.\d+\.\d+\.\d+'`
 set -f
 parts=(${ip//./ })
 team_num=`expr \( \( ${parts[1]} - 60 \) * 256 \) % 1024 + ${parts[2]}`
+
+if [[ -z "$team_num" ]]; then
+	echo Service is note reday to start yet..
+	exit 1
+fi
 
 nick=${1:-@team$team_num}
 master=10.10.10.100:16770
